cd3dc110d3015f4c3e8760e6f6c3c2b1322c6480
imhammadansari <ansarihamad123@gmail.com>
2024-08-10 16:38:48 +0500

    Staged Changes

---
+++ app/page.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './Components/Header'
+import Banner from './Components/Banner'
+import About from './Components/About'
+import Skills from './Components/Skills'
+import Projects from './Components/Projects'
+import Why from './Components/Why'
+import Contact from './Components/Contact'
+import Footer from './Components/Footer'
+import Message from './Components/RestaurentMessage'
+
+const page = () => {
+  return (
+    <>
+    <Header />
+    <Banner />
+    <About />
+    <Skills />
+    <Projects />
+    <Why />
+    <Contact />
+    <Footer />
+    
+    </>
+  )
+}
+
+export default page
