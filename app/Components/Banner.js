8826d3ed5e6a8e91fedea483a8c917a9cb6df787
hammad084 <hraservices123@gmail.com>
2025-02-18 00:12:55 +0500

    changes

--- app/Components/Banner.js
+++ app/Components/Banner.js
@@ -17,17 +17,19 @@ const Banner = () => {
 
   return (
     <>
-    <div className='w-full bg-black bg-opacity-95 h-[25rem]'>
-
-        <div className=' w-full h-full'></div>
+    <div className='relative w-full h-auto'>
+      <div className=''>
+        <img className='w-full h-full ' src='./new.jpg' />
+        <div className='bg-black absolute inset-0 w-full h-full opacity-50'></div>
         
-        <div className='absolute inset-0 text-center text-white my-10 md:my-20 lg:my-36 xl:my-36 w-full'>
+        <div className='absolute inset-0 text-white mx-4 my-10 sm:m md:mx-12 md:my-20 lg:mx-24 lg:my-36 xl:mx-24 xl:my-36 w-[18rem] sm:w-[22rem] md:w-[26rem] lg:w-[40rem] xl:w-[40rem]'>
         <h1 className='text-sm sm:text-base md:text-xl lg:text-3xl xl:text-3xl font-bold' data-aos="fade-left">Hi, I'm &lt;/&gt; <br/> <span className='text-sky-600 text-2xl sm:text-2xl md:text-4xl lg:text-6xl xl:text-6xl font-bold animate__animated animate__backInLeft'>Hammad Ansari</span></h1>
         <h1 className='text-sm md:text-base lg:text-xl xl:text-xl lg:my-1 xl:my-1 text-white' data-aos="fade-up">MERN Stack Developer</h1>
 
         <button onClick={() => document.getElementById('contact').scrollIntoView({ behavior: 'smooth' })} data-aos="fade-left" className='w-[5rem] md:w-[8rem] lg:w-32 xl:w-32 h-7 md:h-9 lg:h-10 xl:h-10 bg-sky-600 text-white mt-2 text-xs md:text-base lg:text-lg xl:text-lg hover:bg-white hover:text-sky-600 rounded'>Contact Me</button>
         </div>
 
+      </div>
     </div>
     
     </>
